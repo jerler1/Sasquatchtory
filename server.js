07e0bb2f9df3cbb614dcf543ced595b56210b0da
Bunks <busk_1@yahoo.com>
2021-06-12 15:31:26 -0400

    added express and mongoose and made a basic server

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ entended: true }));
+app.use(express.json());
+
+app.use(express.static("client/build"));
+
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/sasquatchtory", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
+
+  if (process.env.PRODUCTION_URL) {
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "client/build/index.html"));
+    });
+  }
+
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
