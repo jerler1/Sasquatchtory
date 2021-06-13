bbee1be131a2e858f5be426f98b05326d0fb5ae5
Bunks <busk_1@yahoo.com>
2021-06-13 19:56:02 -0400

    added basic not found page

---
+++ client/views/NotFound/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./NotFound.scss";
+
+const notFound = () => {
+    return (
+        <div className="notFoundContainer">
+            <h1>Page not found!</h1>
+        </div>
+    );
+};
+
+export default notFound;
