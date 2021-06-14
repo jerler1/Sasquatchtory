3272cc2de9826de7c921578a3e4d43c7c1313319
Bunks <busk_1@yahoo.com>
2021-06-14 19:05:44 -0400

    added profile names to the profile list

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import "./Navbar.scss";
 import { Link } from "react-router-dom";
+import api from "../../api/index";
 
 const Navbar = () => {
-    return (
-        <nav className="navbarContainer">
-            <div className="brand-logo">
-                <Link to="/">
-                    <h4>Sasquatchtory</h4>
-                </Link>
-            </div>
-        </nav>
-    );
+  const [activeProfile, setActiveProfile] = useState(null);
+  const [isDropdownActive, setIsDropdownActive] = useState(false);
+  const [listOfProfiles, setListOfProfiles] = useState([]);
+
+  useEffect(() => {
+    api.getProfiles().then((res) => setListOfProfiles(res.data));
+  }, []);
+
+  return (
+    <nav className="navbarContainer">
+      <div className="brand-logo">
+        <Link to="/">
+          <h4>Sasquatchtory</h4>
+        </Link>
+      </div>
+      <div className="nav-links">
+        <button className="newProfileButton button">
+          <p>New Profile</p>
+        </button>
+        <div className={isDropdownActive ? "dropdown is-active" : "dropdown"}>
+          <div className="dropdown-trigger">
+            <button
+              className="button"
+              aria-haspopup="true"
+              aria-controls="dropdown-menu2"
+              onClick={() => setIsDropdownActive(!isDropdownActive)}
+            >
+              <span>
+                <p>Choose Profile</p>
+              </span>
+              <span className="icon is-small">
+                <i className="fas fa-angle-down" aria-hidden="true"></i>
+              </span>
+            </button>
+          </div>
+          <div className="dropdown-menu">
+            {listOfProfiles.map((profile) => {
+              return (
+                <div className="dropdown-content">
+                  <p>{profile.name}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
