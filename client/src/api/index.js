6897ed818a2c6722296e5e144e550c27f7db8828
Bunks <busk_1@yahoo.com>
2021-06-19 21:20:25 -0400

    added getprofilebyid and update profile api calls

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,12 +1,18 @@
 import axios from "axios";
 
 const api = {
-    getProfiles() {
-        return axios.get("http://localhost:3001/api/profile/");
-    },
-    makeFactory(form) {
-        return axios.post("http://localhost:3001/api/factory", form).then(({data}) => data);
-    }
+  getProfiles() {
+    return axios.get("http://localhost:3001/api/profile/");
+  },
+  getProfileById(id) {
+    return axios.get("http://localhost:3001/api/profile/" + id);
+  },
+  updateProfile(id, profileData) {
+    return axios.put(`http://localhost:3001/api/profile/${id}`, profileData);
+  },
+  makeFactory(form) {
+    return axios.post("http://localhost:3001/api/factory", form);
+  },
 };
 
 export default api;
