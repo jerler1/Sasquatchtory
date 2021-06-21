a84b663a13d9de39d0ebc4c626b99eeacc3593ba
Bunks <busk_1@yahoo.com>
2021-06-20 22:15:52 -0400

    added a number display component

---
+++ client/src/components/NumberDisplay/NumberDisplay.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./NumberDisplay.scss";
+
+const NumberDisplay = () => {
+    return (
+        <div className="numberContainer">
+            <h3>3</h3>
+        </div>
+    );
+};
+
+export default NumberDisplay;
